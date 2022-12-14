6b6211abfd9ceb9c854e808dc84e6743cb0456a2
Artur Nikolaichuk <59363088+nikolaichukkk@users.noreply.github.com>
2022-12-14 23:21:41 +0200

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
